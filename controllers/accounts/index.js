1c21446ac55d323130a0fea91467bb0994f252f5
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2015-11-03 00:22:04 +0100

    KnP: cleaning some redundant codes

--- controllers/accounts/index.js
+++ controllers/accounts/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var Account = req.models.account;
-  Account.all(function (err, accounts) {
+  Account.find(function (err, accounts) {
     if (err) return next(err);
     console.log(accounts);
     res.render("list", {accounts: accounts});
