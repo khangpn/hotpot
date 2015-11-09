2854b6dc545a7f568a1361e32513004ec4ff93d9
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2015-11-09 13:34:40 +0100

    Code: rename settings.js to config/db_config.js

---
+++ config/db_config.js
@@ -0,0 +1,22 @@
+var settings = {
+  development: {
+    database : "hotpot",
+    username : "hotpot",
+    password : "hotpot",
+    options  : {
+      dialect : "postgresql",
+      host     : "127.0.0.1"
+    }
+  },
+  production: {
+    database : "hotpot",
+    username : "hotpot",
+    password : "hotpot",
+    options  : {
+      dialect : "postgresql",
+      host     : "127.0.0.1"
+    }
+  }
+};
+
+module.exports = settings;
--- models/index.js
+++ models/index.js
@@ -4,14 +4,16 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
-var db_config = require(__dirname + '/../settings.js')[env]['database'];
+var db_config = require(__dirname + '/../config/db_config.js')[env];
 var sequelize = new Sequelize(db_config.database, db_config.username, db_config.password, db_config.options);
 var db        = {};
 
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
+    return (file.indexOf(".") !== 0) 
+      && (path.extname(file) === ".js")
+      && (file !== "index.js");
   })
   .forEach(function(file) {
     var model = sequelize.import(path.join(__dirname, file));
