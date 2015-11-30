fbd3e493be78ae0dd3c16e1985da3d2ed37541b7
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2015-11-30 15:53:56 +0100

    Code: Update account password

--- controllers/accounts/index.js
+++ controllers/accounts/index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  var Account = req.models.account;
-  Account.findAll({include: req.models.account_detail})
-    .then(function(accounts){
-        res.render("list", {accounts: accounts});
-      }, 
-      function(error){
-        return next(error);
-    });
-});
-
+//------------------- Admin Section ----------------------
 router.get('/create', function(req, res, next) {
   res.render("create");
 });
@@ -32,10 +21,42 @@ router.post('/create', function(req, res, next) {
       });
     });
 });
+//--------------------------------------------------------
 
-router.post('/update', function(req, res, next) {
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var Account = req.models.account;
+  Account.findAll({include: req.models.account_detail})
+    .then(function(accounts){
+        res.render("list", {accounts: accounts});
+      }, 
+      function(error){
+        return next(error);
+    });
 });
 
+router.get('/:id', function (req, res, next) {
+  var Account = req.models.account;
+  Account.findById(req.params.id, {include: [req.models.account_detail]})
+    .then(function(account) {
+        if (!account) return next(new Error("Can't find the account with id: " + req.params.id));
+        account.getProjects()
+          .then(function (projects) {
+              res.render('view', {
+                account: account,
+                projects: projects
+              }); 
+            }, function (errors) {
+              return next(error);
+            }
+          );
+      }, 
+      function(error) {
+        return next(error);
+    });
+});
+
+//------------------- Owner section ----------------------
 router.post('/updateDetail/:id', function(req, res, next) {
   var data = req.body;
   var Account = req.models.account;
@@ -81,25 +102,46 @@ router.post('/updateDetail/:id', function(req, res, next) {
     );
 });
 
-router.get('/:id', function (req, res, next) {
+//NOTE: after handling login, remove :id from this link
+router.get('/update_password/:id', function(req, res, next) {
   var Account = req.models.account;
   Account.findById(req.params.id, {include: [req.models.account_detail]})
     .then(function(account) {
         if (!account) return next(new Error("Can't find the account with id: " + req.params.id));
-        account.getProjects()
-          .then(function (projects) {
-              res.render('view', {
-                account: account,
-                projects: projects
-              }); 
-            }, function (errors) {
-              return next(error);
-            }
-          );
+        res.render('update_password', {
+          account: account
+        }); 
       }, 
       function(error) {
         return next(error);
     });
 });
+
+router.post('/update_password', function(req, res, next) {
+  if (!req.body) return next(new Error('Cannot get the req.body'));
+
+  var data = req.body;
+  var Account = req.models.account;
+
+  Account.findById(data.id)
+    .then(function(account) {
+        if (!account) return next(new Error("Can't find the account with id: " + req.params.id));
+        account.set('password', data.password);
+        account.set('password_confirm', data.password_confirm);
+        account.save()
+          .then(function(updatedAcc){
+            res.redirect('/accounts/' + updatedAcc.id);
+          }, function(error){
+            return res.render("update_password", {
+              account: account,
+              error: error
+            });
+          });
+       }, 
+      function(error) {
+        return next(error);
+    });
+});
+//--------------------------------------------------------
 
 module.exports = router;
