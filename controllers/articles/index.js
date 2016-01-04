071e92b28b220807c70e17be66b760349c42077b
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2016-01-04 14:20:11 +0100

    Code: update article AC logic

--- controllers/articles/index.js
+++ controllers/articles/index.js
@@ -41,8 +41,6 @@ router.get('/delete/:id',
 /*
 * NOTE: Owner can edit his article as long as his level is
 lower than its.
-* Other users can only edit if he belongs to the project where
-the article is created, and his level is lower than its
 */
 router.get('/edit/:id',
   function(req, res, next) {
@@ -58,7 +56,9 @@ router.get('/edit/:id',
       .then(function(article) {
           if (!article) return next(new Error("Can't find the article with id: " + req.params.id));
           // if user is owner or the aritcle is open for writing
-          if (article.writable || article.account_id == current_account.id) {
+          //if (article.writable || article.account_id == current_account.id) {
+          // only owner can edit
+          if (article.account_id == current_account.id) {
             current_account.getProjectProfiles({
               where: {
                 project_id: article.project.id
@@ -69,9 +69,11 @@ router.get('/edit/:id',
               function(project_profiles) {
                 if (project_profiles.length > 0) {
                   var project_profile = project_profiles[0];
+                  // if user level is lower than article level
                   if ( project_profile.security_level &&
                     project_profile.security_level.level <=
                     article.security_level.level) {
+                    res.locals.current_profile = project_profile;
                     res.locals.current_article = article;
                     return next();
                   }
@@ -90,9 +92,13 @@ router.get('/edit/:id',
   },
   function(req, res, next) {
     var article = res.locals.current_article;
+    var project_profile= res.locals.current_profile ;
     var SecurityLevel = req.models.security_level;
-    SecurityLevel.findAll()
-      .then(function(security_levels){
+    SecurityLevel.findAll(
+      { where: {
+          level: { $gte: project_profile.security_level.level }
+      } }
+    ).then(function(security_levels){
         res.render('edit', {
           article: article,
           security_levels: security_levels
@@ -107,7 +113,8 @@ router.post('/update',
   function(req, res, next) {
     if (!res.locals.authenticated) return util.handle_unauthorized(next);
     if (!req.body) return next(new Error('Cannot get the req.body'));
-    var article_id = req.body.id;
+    var data = req.body;
+    var article_id = data.id;
     var Article = req.models.article;
     var SecurityLevel = req.models.security_level;
     var Project = req.models.project;
@@ -118,7 +125,9 @@ router.post('/update',
       .then(function(article) {
           if (!article) return next(new Error("Can't find the article with id: " + req.params.id));
           // if user is owner or the aritcle is open for writing
-          if (article.writable || article.account_id == current_account.id) {
+          //if (article.writable || article.account_id == current_account.id) {
+          // only owner can edit
+          if (article.account_id == current_account.id) {
             current_account.getProjectProfiles({
               where: {
                 project_id: article.project.id
@@ -128,15 +137,37 @@ router.post('/update',
             }).then(
               function(project_profiles) {
                 if (project_profiles.length > 0) {
+                  // if user level is lower than article level
                   var project_profile = project_profiles[0];
                   if ( project_profile.security_level &&
                     project_profile.security_level.level <=
                     article.security_level.level) {
+                    res.locals.current_profile = project_profile;
                     res.locals.current_article = article;
-                    return next();
+                    // the article security_level is changed
+                    if (data.security_id != article.security_level.id) {
+                      SecurityLevel.findById(data.security_level_id)
+                      .then(function(security_level) {
+                        if (!security_level) 
+                          return next(new Error("Can't find the security with id: " + data.security_level_id));
+                        if ( project_profile.security_level &&
+                          project_profile.security_level.level <=
+                          security_level.level) {
+                          return next();
+                        }
+                        return util.handle_unauthorized(next);
+                      }, function(error) {
+                        return next(error);
+                      });
+                    } else {
+                      return next();
+                    }
+                  } else {
+                    return util.handle_unauthorized(next);
                   }
+                } else {
+                  return util.handle_unauthorized(next);
                 }
-                return util.handle_unauthorized(next);
               }, function(error) {
                 return next(error);
               });
@@ -152,6 +183,7 @@ router.post('/update',
     var Article = req.models.article;
     var SecurityLevel = req.models.security_level;
     var article = res.locals.current_article;
+    var project_profile= res.locals.current_profile ;
     var data = req.body;
 
     delete data.id;
@@ -159,8 +191,11 @@ router.post('/update',
       .then(function(account) {
         res.redirect('/articles/' + article.id);
       }, function (error) {
-        SecurityLevel.findAll()
-          .then(function(security_levels){
+        SecurityLevel.findAll(
+          { where: {
+              level: { $gte: project_profile.security_level.level }
+          } }
+        ).then(function(security_levels){
             res.render('edit', {
               article: article,
               security_levels: security_levels,
@@ -174,6 +209,7 @@ router.post('/update',
 );
 
 // TODO: check account's roles
+// NOTE: only show the article whose level is lte to current user level
 router.get('/project/:project_id',
   function(req, res, next) {
     if (!res.locals.authenticated) return util.handle_unauthorized(next);
@@ -202,7 +238,10 @@ router.get('/project/:project_id',
   },
   function(req, res, next) {
     var Article = req.models.article;
-    var project_profile= res.locals.current_profile ;
+    var SecurityLevel = req.models.security_level;
+    var project_profile = res.locals.current_profile;
+    // TODO: edit query to select either the article whose security_level is lte to user's
+    // OR the user is its owner
     Article.findAll({
       where: {
         $and: {
@@ -212,7 +251,13 @@ router.get('/project/:project_id',
             { account_id: project_profile.account_id }
           ]
         }
-      }
+      },
+      include: [{
+        model: SecurityLevel
+        where: {
+          level: { $lte: project_profile.security_level.level }
+        }
+      }]
     }).then(
       function(articles) {
         res.render("list", {articles: articles});
@@ -224,6 +269,8 @@ router.get('/project/:project_id',
 //--------------------------------------------------------
 
 //----------------- Authenticated section --------------------
+// NOTE: if the user is a member of the project
+// the user can only create article whose security_level is gte to his
 router.get('/project/:project_id/create',
   function(req, res, next) {
     if (!res.locals.authenticated) return util.handle_unauthorized(next);
@@ -247,7 +294,7 @@ router.get('/project/:project_id/create',
             return next();
           }
         }
-        return util.handle_unauthorized(next);
+        return util.handle_unauthorized(next, 'This user has no security level');
       }, function(error) {
         return next(error);
       });
@@ -291,13 +338,21 @@ router.post('/project/:project_id/create',
       function(project_profiles) {
         if (project_profiles.length > 0) {
           var project_profile = project_profiles[0];
-          if (project_profile.security_level && 
-            project_profile.security_level.level <= data.level) {
-            res.locals.current_profile = project_profile;
-            return next();
-          }
+          SecurityLevel.findById(data.security_level_id)
+          .then(function(security_level) {
+            if (!security_level) return next(new Error("Can't find the security with id: " + data.security_level_id));
+            if (project_profile.security_level && 
+              project_profile.security_level.level <= security_level.level) {
+              res.locals.current_profile = project_profile;
+              return next();
+            }
+            return util.handle_unauthorized(next);
+          }, function(error) {
+            return next(error);
+          });
+        } else {
+          return util.handle_unauthorized(next);
         }
-        return util.handle_unauthorized(next);
       }, function(error) {
         return next(error);
       });
